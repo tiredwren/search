88a58d87a7fde44013fac3fa228ce96f5bf5a48d
Sneha M <moudgalya.sneha@gmail.com>
2023-03-04 15:38:13 -0800

    more data

--- search/app/src/main/java/com/example/search/ItemsModal.kt
+++
@@ -1,9 +0,0 @@
-package com.example.search
-
-data class ItemsModal(
-    // on below line we are creating a two variable
-    // one for course name and other for course image.
-    var name: String,
-    var desc: String,
-    var image: Int
-)
